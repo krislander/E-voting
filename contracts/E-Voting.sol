9cd6b90723544312e8733469f354d3756c28578a
Kristian Kirilov <zoro1503@abv.bg>
2021-02-06 12:37:02 +0200

    Smart contract main functionality and deployed to local blockchain

--- contracts/E-Voting.sol
+++ contracts/E-Voting.sol
@@ -8,23 +8,39 @@ contract Election {
     }
     
     struct Candidate {
-        uint256 candidateId;
-        bytes32 candidateName;
-        bytes32 candidateParty;
+        uint32 candidateId;
+        string candidateName;
+        string candidateParty;
         uint256 votesReceived;
     }
 
-    
     struct Voter {
         address voterAddress;
+        uint32 voterId;
+        bool hasVoted;
     }
+
+    address private owner;
+    modifier isOwner {
+        require(msg.sender == owner, "You are not the owner of the Election!");
+        _;
+    }
+
+    mapping (uint32 => Candidate) public candidates;
+    mapping (address => Voter) public voters;
+    uint32 public candidatesCount;
+    uint32 public votersCount;
     
-    mapping (uint32 => Candidate) private candidates;
-    mapping (uint256 => Voter) private voters;
-    uint32 private candidatesCount;
-    uint32 private votersCount;
-    
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+    event AddCandidate(uint candidateId);
     event VoteInElection(uint256 indexed candidateId);
+    
+    constructor() {
+        owner = msg.sender;
+        emit OwnerSet(address(0), owner);
+        addCandidate("Donald Trump", "Republicans");
+        addCandidate("Joe Biden", "Democrats");
+    }
     
     function getNumberOfCandidates() external view returns(uint32) {
         return candidatesCount;
@@ -34,16 +50,29 @@ contract Election {
         return votersCount;
     }
     
-    function getCandidate(uint32 candidateId) public view returns(uint256, bytes32, bytes32, uint256) {
+    function getCandidate(uint32 candidateId) public view returns(uint256, string memory, string memory, uint256) {
         return (candidateId, candidates[candidateId].candidateName, candidates[candidateId].candidateParty, candidates[candidateId].votesReceived);
     }
-    
-    function addCandidate(bytes32 name, bytes32 party) private {
-        candidates[candidatesCount] = Candidate(candidatesCount + 1, name, party, 0);
-        candidatesCount++;
+
+    function getTotalVotes(uint32 candidateId) view public returns (uint256) {
+        bytes memory nameString = bytes(candidates[candidateId].candidateName);
+        require(candidateId > 0 && nameString.length == 0, "Invalid candidate ID");
+        return candidates[candidateId].votesReceived;
     }
     
-    function vote(uint256 candidateId) public {
-        
+    function addCandidate(string memory name, string memory party) isOwner public {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, name, party, 0);
+        emit AddCandidate(candidatesCount);
+    }
+    
+    function vote(uint32 candidateId) public {
+        // require(!voters[msg.sender], "Candidate has already voted!");
+        require(candidateId > 0 && candidateId <= candidatesCount, "Invalid Candidate!");
+
+        uint32 voterId = votersCount++;
+        candidates[candidateId].votesReceived++;
+        voters[msg.sender] = Voter(msg.sender, voterId, true);
+        emit VoteInElection(candidateId);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+// const ConvertLib = artifacts.require("ConvertLib");
+// const MetaCoin = artifacts.require("MetaCoin");
+const Election = artifacts.require("Election");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  // deployer.deploy(ConvertLib);
+  // deployer.link(ConvertLib, MetaCoin);
+  // deployer.deploy(MetaCoin);
+  deployer.deploy(Election);
 };
